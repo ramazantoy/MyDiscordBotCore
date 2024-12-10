c0d770181599b16dc8c9bb8536f20b590a8eb7e7
Ramazan  TOY <ramcotoy06@gmail.com>
2024-12-10 23:33:37 +0300

    removed all files

--- Interfaces/IBot.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.Extensions.DependencyInjection;
-
-namespace DiscordBot.Interfaces;
-
-public interface IBot
-{
-    Task StartAsync(ServiceProvider services);
-    Task StopAsync();
-}
